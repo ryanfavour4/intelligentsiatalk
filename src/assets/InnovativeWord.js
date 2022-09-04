257f376d0707f864480db717788c4a5bffe4cfdb
ryanfavour4 <ryanfave022@gmail.com>
2022-09-04 21:32:00 +0100

    second upload

---
+++ src/assets/InnovativeWord.js
@@ -0,0 +1,30 @@
+const InnovativeWord = () => {
+    return ( 
+        <div className="section-bgblue innovative-bg">
+
+            <div className="intelligentsiaBrand-left">
+                <img src="./images/intelligentsiatalklogo.png" alt="" />
+            </div>
+            <div className="container">
+            
+            <hr className="cutrule" />
+
+            <div className="gold-blade"></div>
+            <div className="blue-blade"></div>
+            
+                <div className="inner-word-writeup">
+                    <div className="heading-innovative-words">
+                       <h1> FEAR KILLS MORE DREAMS <br />
+                         THAN FAILURE EVER WILL. 
+                       </h1>
+                    </div>
+                    <div className="sub-heading-innovative-words">
+                    Inaction breeds doubt and fear. Action breeds confidence and courage. If you want to conquer fear, do not sit home and think about it. Go out and get busy.
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default InnovativeWord;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 
+import Loadscreen from "../components/Loadscreen";
+
 import Header from "../assets/Header";
 import Section1 from "../assets/Section1";
 import Wordingswall from "../assets/Wordingswall";
-import Loadscreen from "../components/Loadscreen";
+import InnovativeWord from "../assets/InnovativeWord";
+
 const Home = () => {
 
     let [loadscreen, setLoadscreen] = useState(true)
@@ -20,6 +23,7 @@ const Home = () => {
             <Header/>
             <Section1/>
             <Wordingswall/>
+            <InnovativeWord/>
         </div>
     )
 }
